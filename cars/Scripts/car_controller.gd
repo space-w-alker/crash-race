1f0aa54b2bb2903ab90b38f7dd6ede5d99ae9309
Abdullahi Abass <abassabdullahi4@gmail.com>
2024-09-24 10:15:18 +0100

    feat: updates

--- cars/Scripts/car_controller.gd
+++ cars/Scripts/car_controller.gd
@@ -8,6 +8,7 @@ var steer: float = 0;
 
 var brake_target: float = 0;
 var last_balance_time: float = 0
+var last_angular_velocity: Vector3 = Vector3.ZERO;
 
 @onready var floorRay: RayCast3D = $detect_floor
 @onready var wallRay: RayCast3D = $detect_wall
@@ -50,10 +51,16 @@ func reverse():
 func set_steer(value: float):
 	steer = value
 
-func _physics_process(delta: float) -> void:
+func _ready() -> void:
+	add_to_group("cars")
+	pass
 
-	if (angular_velocity.length_squared() > 5 and Time.get_ticks_msec() - last_balance_time > 1000):
-		print("Too much: ", angular_velocity.length_squared(), "Applying force")
+
+func _physics_process(delta: float) -> void:
+	last_angular_velocity = angular_velocity;
+	if (angular_velocity.length_squared() > 2 and Time.get_ticks_msec() - last_balance_time > 1000):
+		pass
+		# print("Too much: ", angular_velocity.length_squared(), "Applying force")
 	engine_force = lerpf(engine_force, target_engine_force, weight * delta)
 	steering = lerpf(steering, steer * 0.3, 30 * delta)
 	brake = lerpf(brake, brake_target, 30 * delta)
